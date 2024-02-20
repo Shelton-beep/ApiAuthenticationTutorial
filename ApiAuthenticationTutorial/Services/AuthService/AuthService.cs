0dd28281323a7a01e2b6ef5adc826543771814e4
Shelton Simbi <shltnsimbi@gmail.com>
2024-02-20 15:17:17 +0200

    Created a userchangePassword model and its service implementation

--- ApiAuthenticationTutorial/Services/AuthService/AuthService.cs
+++ ApiAuthenticationTutorial/Services/AuthService/AuthService.cs
@@ -116,5 +116,30 @@ namespace ApiAuthenticationTutorial.Services.AuthService
             
             return jwt;
         }
+
+        public async Task<ServiceResponse<bool>> ChangePassword(int userId, string newPassword)
+        {
+            var user = await _context.Users.FindAsync(userId);  
+            if(user == null)
+            {
+                return new ServiceResponse<bool>
+                {
+                    IsSuccess = false,
+                    Message = "User not found"
+                };
+            }
+
+            CreatePasswordHash(newPassword, out byte[] passwordHash, out byte[] passwordSalt);
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await _context.SaveChangesAsync();
+
+            return new ServiceResponse<bool>
+            {
+                Data = true,
+                Message = "Password has been changed"
+            };
+        }
     }
 }
