0caf579d61f71c33839abb11f280437ddd1053a5
Shelton Simbi <shltnsimbi@gmail.com>
2024-02-20 16:29:41 +0200

    Added authentication middleware

--- ApiAuthenticationTutorial/Program.cs
+++ ApiAuthenticationTutorial/Program.cs
@@ -3,6 +3,8 @@ global using Microsoft.EntityFrameworkCore;
 global using ApiAuthenticationTutorial.Services.UserServices;
 global using ApiAuthenticationTutorial.Data;
 global using ApiAuthenticationTutorial.Services.AuthService;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,6 +20,18 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8
+                .GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value)),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+        };
+    });
 
 var app = builder.Build();
 
@@ -29,6 +43,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
