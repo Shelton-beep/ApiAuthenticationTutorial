83001b8fe252119d5c06cc4a6a59bbb510680325
Shelton Simbi <shltnsimbi@gmail.com>
2024-02-20 12:35:55 +0200

    Created a database migration

--- ApiAuthenticationTutorial/Program.cs
+++ ApiAuthenticationTutorial/Program.cs
@@ -1,11 +1,15 @@
 global using ApiAuthenticationTutorial.Models;
 global using Microsoft.EntityFrameworkCore;
+using ApiAuthenticationTutorial.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
